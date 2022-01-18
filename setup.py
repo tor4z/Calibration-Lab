b691f7b8adab176d4121ce4e371314b225ebc122
tor4z <vwenjie@hotmail.com>
2022-01-18 11:01:32 +0800

    Update project

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import datetime
+
+
+def gen_code():
+    d = datetime.datetime.now()
+    date_str = d.strftime('%Y%m%d%H%M%S')
+    return f'dev{date_str}'
+
+
+__version__ = f'0.0.1-{gen_code()}'
+
+
+setup(name='calib',
+      version=__version__,
+      description='Calibration lab',
+      author='tor4z',
+      author_email='vwenjie@hotmail.com',
+      install_requires=[
+            'PyYAML'
+      ],
+      packages=['cfg'],
+)
